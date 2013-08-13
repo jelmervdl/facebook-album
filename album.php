bacb07a8d272a5fb46bab5c8e10e2e156bde1ae7
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2013-08-13 16:36:15 +0200

    Rewrite viewer code in CoffeeScript

--- album.php
+++ album.php
@@ -42,109 +42,15 @@ $photos = $album->listPhotos();
 				_html($photo->caption())); ?>
 		<?php endforeach ?>
 		</ol>
-		<script>
-			var overlay = document.createElement('figure');
-			overlay.id = 'overlay';
+		
+		<figure id="overlay">
+			<img class="background">
+			<img class="full-size">
+			<figcaption id="caption"></figcaption>
+			<button class="next-button">Next</button>
+			<button class="prev-button">Prev</button>
+		</figure>
 
-			var large = document.createElement('img');
-			overlay.appendChild(large);
-
-			var caption = document.createElement('figcaption');
-			overlay.appendChild(caption);
-
-			var next = document.createElement('button');
-			next.id = 'next-button';
-			next.appendChild(document.createTextNode('Volgende'));
-			overlay.appendChild(next);
-
-			var prev = document.createElement('button');
-			prev.id = 'prev-button';
-			prev.appendChild(document.createTextNode('Vorige'));
-			overlay.appendChild(prev);
-
-			document.body.appendChild(overlay);
-
-			overlay.addEventListener('click', function(e) {
-				e.preventDefault();
-
-				overlay.classList.remove('visible');
-			});
-
-			var currentPhoto = null;
-
-			next.addEventListener('click', function(e) {
-				e.stopPropagation();
-
-				var nextPhoto = findPhoto(currentPhoto, +1);
-
-				if (nextPhoto)
-					showPhoto(nextPhoto);
-			});
-
-			prev.addEventListener('click', function(e) {
-				e.stopPropagation();
-
-				var prevPhoto = findPhoto(currentPhoto, -1);
-
-				if (prevPhoto)
-					showPhoto(prevPhoto);
-			});
-
-			var findPhoto = function(photo, offset)
-			{
-				var photos = document.querySelectorAll('.photo a');
-
-				for (var i = 0; i < photos.length; ++i)
-					if (photos[i] == photo)
-						return photos[i + offset] || null;
-
-				return null;
-			}
-
-			var showPhoto = function(photo)
-			{
-				// Keep track of which photo is now shown for next & prev links.
-				currentPhoto = photo;
-
-				// Set large image to the link target.
-				large.width = photo.getAttribute('data-width');
-				large.height = photo.getAttribute('data-height');
-				large.src = photo.href;
-
-				// Also set caption.
-				caption.innerHTML = photo.querySelector('figcaption').innerHTML;
-
-				// Show large image.
-				overlay.classList.add('visible');
-
-				// Preload next 2 photo's.
-				for (var i = 1; i <= 2; ++i)
-				{
-					var nextPhoto = findPhoto(photo, i);
-				
-					if (!nextPhoto) break;
-
-					preloadPhoto(nextPhoto);
-				}
-			}
-
-			var preloadPhoto = function(photo)
-			{
-				var img = new Image();
-				img.src = photo.href;
-			}
-
-			Array.prototype.forEach.call(
-				document.querySelectorAll('.photo a'),
-				function(photo) {
-					photo.addEventListener('click', function(e) {
-						e.preventDefault();
-
-						showPhoto(photo);
-					});
-				}
-			);
-		</script>
 		<script src="js/app.js"></script>
 		<script>app_init()</script>
 	</body>
--- coffee/app.coffee
+++ coffee/app.coffee
@@ -6,7 +6,10 @@ class Photo
     @link = @el.querySelector 'a'
     @img = @el.querySelector 'img'
 
-    @img.src = @link.href
+    @url = @link.href
+    @thumbURL = @img.src;
+
+    @img.src = @url
     @img.setAttribute 'width', '100%'
     @img.setAttribute 'height', '' #auto
     
@@ -14,9 +17,80 @@ class Photo
     @height = @link.getAttribute 'data-height'
     @aspect_ratio = @width / @height
 
+  caption: ->
+    @link.querySelector('figcaption').innerHTML
+
   resize: (dimensions) ->
     @el.style.width = "#{dimensions.width}px"
     @el.style.height = "#{dimensions.height}px"
+
+
+class Viewer
+  constructor: (@photos) ->    
+    @el = document.getElementById 'overlay'
+
+    @image = @el.querySelector '.full-size'
+    @background = @el.querySelector '.background'
+    @caption = @el.querySelector 'figcaption'
+
+    @nextButton = @el.querySelector '.next-button'
+    @prevButton = @el.querySelector '.prev-button'
+
+    @el.addEventListener 'click', (evt) =>
+      if evt.target == @el or evt.target == @image
+        @hide()
+
+    @nextButton.addEventListener 'click', (evt) =>
+      evt.preventDefault()
+      @next()
+
+    @prevButton.addEventListener 'click', (evt) =>
+      evt.preventDefault()
+      @prev()
+
+    document.addEventListener 'keyup', (evt) =>
+      if @el.classList.contains 'visible'
+        if evt.keyCode == 27
+          evt.preventDefault()
+          @hide()
+
+        if evt.keyCode == 39
+          evt.preventDefault()
+          @next()
+
+        if evt.keyCode == 37
+          evt.preventDefault()
+          @prev()
+
+  show: (photo) ->
+    @current = @photos.indexOf photo
+    console.assert (@current != -1), 'Photo not found in list'
+    @render()
+
+  hide: ->
+    @el.classList.remove 'visible'
+
+  next: ->
+    @current++
+    @render()
+
+  prev: ->
+    @current--
+    @render()
+
+  render: ->
+    photo = @photos[@current]
+
+    @el.className = if photo.aspect_ratio > 1 then 'horizontal' else 'vertical'
+
+    @image.src = photo.url
+    @background.src = photo.thumbURL
+    @caption.innerHTML = photo.caption()
+
+    @nextButton.disabled = @current == @photos.length - 1
+    @prevButton.disabled = @current == 0
+
+    @el.classList.add 'visible'
 
 
 order_photos = (photos, viewport_width, ideal_height) ->
@@ -57,6 +131,14 @@ layout_photos = (photos) ->
 
 window.app_init = ->
   photos = gather_photos()
+  viewer = new Viewer photos
+
+  # Listen for clicks on photo's
+  for photo in photos
+    do (photo) ->
+      photo.link.addEventListener 'click', (evt) ->
+        evt.preventDefault();
+        viewer.show photo
 
   layout_timeout = null
   window.onresize = ->
